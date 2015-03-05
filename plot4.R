0db6bd96ce2eefe706063c5b908cadd1039fd172
Davide Fiorentino <davide.fiorentino@gmail.com>
2015-03-05 23:26:00 +0100

    Add plot 4

--- plot4.R
+++ plot4.R
@@ -16,10 +16,19 @@ subMetering2 <- as.numeric(data$Sub_metering_2)
 subMetering3 <- as.numeric(data$Sub_metering_3)
 
 ## Plot and save to file
-png(file="plot3.png", width=480, height=480)
+png(file="plot4.png", width=480, height=480)
+par(mfrow = c(2, 2))
+
+plot(datetime, data$Global_active_power, type="l", ylab="Global Active Power", xlab="")
+plot(datetime, data$Voltage, type="l", xlab="datetime", ylab="Voltage")
+
 plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
 lines(datetime, subMetering2, type="l", col="red")
 lines(datetime, subMetering3, type="l", col="blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
-       lty=1, lwd=2.5, col=c("black", "red", "blue"), bty="n")
+       lty=, lwd=2.5, col=c("black", "red", "blue"), bty="n")
+
+plot(datetime, data$Global_reactive_power, type="l", xlab="datetime", 
+     ylab="Global_reactive_power")
+
 dev.off()
